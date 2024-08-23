a5f56a035ac79a9c264ec76aa9ee74c0e578977d
selva021196 <gselva810@gmail.com>
2024-08-23 09:25:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <div className='app-navbar'><Navbar /></div>
+    </div>
+  )
+}
+
+export default App

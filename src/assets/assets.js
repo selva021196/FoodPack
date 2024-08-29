715a5d77ca10e05c01f767b29f6f34b7cad9ef5f
selva021196 <gselva810@gmail.com>
2024-08-29 10:57:17 +0530

    added navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,19 @@
+import project_logo1 from './project logo.png'
+import project_logo2 from './project logo2.png'
+import project_logo3 from './project logo3.png'
+import project_logo4 from './project logo4.png'
+import search_icon from './search icon.png'
+import location_logo from './location logo.png'
+import cart_logo from './cart.png'
+import three_dot from './three-dot.png'
+
+export const assets = {
+    project_logo1,
+    project_logo2,
+    project_logo3,
+    project_logo4,
+    search_icon,
+    location_logo,
+    cart_logo,
+    three_dot
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets.js'
+
+
+
 const Navbar = () => {
+  const [isVisible,setisVisible] = useState(false)
+  const handle_visible = () =>{
+    setisVisible(!isVisible)
+  }
+
+
   return (
     <div className='navbar'>
+        <div className='title-logo' ><img src={ assets.project_logo4 } alt="" />
+        <div onClick={handle_visible} className='three-dot'><img  src={assets.three_dot} alt="" /></div>
         
-
+        
+        <div className="navbar-mobile">
+           <img src={ assets.location_logo } alt="" />
+           <button className='login'>Login</button>
+            <img src={ assets.cart_logo } className='cart-logo' alt="" /> 
+            </div>
+        </div>
+        <div className={isVisible ? "menu" : "menu-hide"}>
+        <ul >
+          <li>Location</li>
+          <li>Login</li>
+          <li>Carts</li>
+        </ul>
+        </div>
+        <div className="search">
+            <input type="text" className='search-input' />
+           <img src={ assets.search_icon }  alt="" />
+            </div> 
+            
+            <div className="navbar-others">
+           <img src={ assets.location_logo } alt="" />
+           <button className='login'>Login</button>
+            <img src={ assets.cart_logo } className='cart-logo' alt="" /> 
+            </div>
+        
     </div>
   )
 }
